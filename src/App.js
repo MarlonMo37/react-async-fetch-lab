2c4bb108155a05e3d1b8c33bf8b668caf30a156a
MarlonMoreno <marlonmoreno894@gmail.com>
2021-07-23 14:25:14 -0500

    commit tree 5aeae77200d48e1d81e3131b4eb9e429356f61bc

--- src/App.js
+++ src/App.js
@@ -1,1 +1,27 @@
 // create your App component here
+
+import React from 'react'
+
+class App extends React.Component {
+
+    constructor() {
+        super()
+        this.state = {
+            people: []
+        }
+    }
+
+    render() {
+        return(
+            <div></div>
+        )
+    }
+
+    componentDidMount() {
+        fetch('http://api.open-notify.org/astros.json')
+          .then(response => response.json())
+          .then(({people}) => this.setState({ people: people }))
+    }
+}
+
+export default App
